c908e43935dad74c75a7102af1573c3d5fe6ae3e
paco-801 <paco_8489@yahoo.com>
2022-08-01 17:25:20 -0600

    Finished making topbar sticky

--- src/App.js
+++ src/App.js
@@ -4,6 +4,68 @@ function App() {
   return (
     <div className="App">
       <Topbar />
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      test <br/>
+      
     </div>
   );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./topbar.css";
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export default function Topbar() {
   return (
@@ -11,7 +12,19 @@ export default function Topbar() {
           <span className="logo">PAC ADMIN</span>
         </div>
         <div className="topRight">
-          <div className="topbarIcons"><NotificationsNoneIcon /></div>
+          <div className="topbarIconContainer">
+            <NotificationsNoneIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <img src="https://images.pexels.com/photos/12903176/pexels-photo-12903176.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="topAvatar" />
         </div>
       </div>
     </div>
