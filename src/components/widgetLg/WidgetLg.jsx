eabdbd9dccab0f4ce8218ec7b9d077f0eb9c8a4d
paco-801 <paco_8489@yahoo.com>
2022-08-08 18:56:29 -0600

    Stopped right before button types on widgetLg

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,7 +1,23 @@
-import "./widgetLg.css"
+import "./widgetLg.css";
 
 export default function widgetLg() {
   return (
-    <div className="widgetLg">Large</div>
-  )
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Latest Transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="
+            " alt="" className="widgetLgImg" />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -1,7 +1,79 @@
-import "./widgetSm.css"
+import "./widgetSm.css";
+import { Visibility } from "@mui/icons-material";
 
 export default function widgetSm() {
   return (
-    <div className="widgetSm">Small</div>
-  )
+    <div className="widgetSm">
+      <span className="widgetSmTitle">Newly Joined Members</span>
+      <br />
+      <ul className="widgetSmList">
+      <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Andrade 😉</span>
+            <span className="widgetSmUserTitle">Professional Wife</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"/> Display
+          </button>
+        </li><li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Andrade 😉</span>
+            <span className="widgetSmUserTitle">Professional Wife</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"/> Display
+          </button>
+        </li><li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Andrade 😉</span>
+            <span className="widgetSmUserTitle">Professional Wife</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"/> Display
+          </button>
+        </li><li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Andrade 😉</span>
+            <span className="widgetSmUserTitle">Professional Wife</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"/> Display
+          </button>
+        </li><li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Andrade 😉</span>
+            <span className="widgetSmUserTitle">Professional Wife</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"/> Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
